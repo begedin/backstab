9460a8685a678b4eed139764bdf5291b7fee60a0
begedin <begedinnikola@gmail.com>
2015-05-21 22:22:18 +0200

    Used generator to create project scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
-/*global require*/
+/*
+ *  gulpfile.js
+ *  ===========
+ *
+ *  Rather than manage one giant configuration file responsible
+ *  for creating multiple tasks, each task has been broken out into
+ *  its own file in gulp/tasks. Any files in that directory get
+ *  automatically required below.
+ *
+ *  To add a new task, simply add a new task file in that directory.
+ *  gulp/tasks/default.js specifies the default set of tasks to run
+ *  when you run `gulp`.
+ *
+ */
 
-var gulp = require('gulp');
-var sourceMaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
+var requireDir = require('require-dir');
 
-gulp.task('build', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(sourceMaps.init())
-    .pipe(babel())
-    .pipe(sourceMaps.write('.'))
-    .pipe(gulp.dest('tmp'));
-});
+
+// Specify game project paths for tasks.
+global[ 'paths' ] = {
+    'src': 'src',
+    'assets' : 'assets',
+    'temp'   : '.tmp',
+    'product': 'dist',
+
+    get less    () { return this['src'] + '/less/*.less'; },
+    get scripts () { return this['src'] + '/scripts/**/*.js'; }
+};
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
